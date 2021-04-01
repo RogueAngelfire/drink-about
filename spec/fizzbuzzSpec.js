5bb96806475a106e5a020960753c759bf306f39a
Robin Collins <info@robincollins.co.uk>
2021-04-01 14:37:38 +0000

    pushing past commit

--- spec/fizzbuzzSpec.js
+++ spec/fizzbuzzSpec.js
@@ -15,21 +15,21 @@ describe("My fizzBuzz function", function() {
         });
         
         it("should return Fizz when called as fizzBuzz(9)", function() {
-            var result = fizzBuzz(9)
+            let result = fizzBuzz(9)
             expect(result).toBe("Fizz");
         });
                 
         it("should return Buzz when called as fizzBuzz(10)", function() {
-            var result = fizzBuzz(10)
+            let result = fizzBuzz(10)
             expect(result).toBe("Buzz");
         });
         
         it("should return FizzBuzz when called as fizzBuzz(15)", function() {
-            var result = fizzBuzz(15)
+            let result = fizzBuzz(15)
             expect(result).toBe("FizzBuzz");
         });
         it("should return 2 when called as fizzBuzz(2)", function() {
-            var result = fizzBuzz(2)
+            let result = fizzBuzz(2)
             expect(result).toBe(2);
         });
     });
